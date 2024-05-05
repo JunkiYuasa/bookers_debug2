15162c5a4b46a6d685cb2d9ab836f330b38bfd17
JunkiYuasa <165625194+JunkiYuasa@users.noreply.github.com>
2024-05-05 14:53:01 +0000

    DM機能の実装（メッセージ送信時にエラーあり）

--- bookers_debug2/app/controllers/books_controller.rb
+++ bookers_debug2/app/controllers/books_controller.rb
@@ -4,7 +4,6 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @book_new = Book.new
     @book_comment = BookComment.new
-    
   end
 
   def index
---
+++ bookers_debug2/app/controllers/rooms_controller.rb
@@ -0,0 +1,22 @@
+class RoomsController < ApplicationController
+  before_action :authenticate_user!
+  
+  def create
+    room = Room.create
+    current_entry = Entry.create(user_id: current_user.id, room_id: room.id)
+    another_entry = Entry.create(user_id: params[:entry][:user_id], room_id: room.id)
+    redirect_to room_path(room)
+  end
+  
+  def index
+  end
+  
+  def show
+    @room = Room.find(params[:id])
+    @messages = @room.messages
+    @message = Message.new
+    @entries = @room.entries
+    @another_entry = @entries.where.not(user_id: current_user.id)
+  end
+end
+
